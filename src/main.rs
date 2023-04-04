386e101dbb078181e9f63cb25a845fdfc77e15fc
Tijme <tijmevh@gmail.com>
2023-04-04 23:25:12 +0200

    First commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use bpaf::Bpaf;
+
+#[derive(Debug, Clone, Bpaf)]
+#[bpaf(options)]
+struct Args {
+    #[bpaf(short('f'), long("fahrenheit"))]
+    /// Whether to convert from fahrenheit instead of celsius
+    fahrenheit: bool,
+    #[bpaf(positional("degrees"))]
+    degrees: f64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = args().run();
+
+    let degrees = if args.fahrenheit {
+        (args.degrees - 32.0) * 5.0 / 9.0
+    } else {
+        args.degrees * 9.0 / 5.0 + 32.0
+    };
+
+    let unit = if args.fahrenheit { "°C" } else { "°F" };
+
+    println!("{}{}", degrees, unit);
 }
